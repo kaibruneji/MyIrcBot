0898a86583da60ce35d7a768b5174c3018dc4ae3
kaibruneji <kaibruneji@gmail.com>
2022-06-25 23:55:34 +0300

    try fix bug ipwhois

--- whois.py
+++ whois.py
@@ -2,7 +2,10 @@ from ipwhois import IPWhois
 
 def whois(ip):
     data = IPWhois(ip, timeout = 15)
-    data = data.lookup_whois()    
-    
-    return data
+    data = data.lookup_whois()
+    region = data['nets'][0]['region']
+    country = data['nets'][0]['country']
+    city = data['nets'][0]['city']    
+    address = data['nets'][0]['address']
+    return {'region' region,'city': city, 'country': country, 'address': address}
     
