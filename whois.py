6129d3bc1dbb42194cdc2efd637aeb36a516fbd1
kaibruneji <kaibruneji@gmail.com>
2022-06-26 20:24:21 +0300

    fix ipwhois error

--- whois.py
+++ whois.py
@@ -1,10 +1,13 @@
 from ipwhois import IPWhois
 
 def whois(ip):
-    data = IPWhois(ip, timeout = 15)
-    data = data.lookup_whois()    
-    country = data['nets'][0]['country']
-    city = data['nets'][0]['city']    
-    address = data['nets'][0]['address']
-    return {'city': city, 'country': country, 'address': address}
-    
+    try:
+        data = IPWhois(ip, timeout = 15)
+        data = data.lookup_whois()
+        country = data['nets'][0]['country']
+        city = data['nets'][0]['city']    
+        address = data['nets'][0]['address']
+        return {'city': city, 'country': country, 'address': address}
+    except:
+        ipwhois.exceptions.IPDefinedError:
+            print("***ipwhois.exceptions.IPDefinedError!***")
